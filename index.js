6a508615bc03db301179abf6083fdbbf21915fda
UIT.19521675 <70103187+MaiKhanh19521675@users.noreply.github.com>
2022-11-12 16:07:59 +0700

    five command

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.use((err, req, res, next) => {
 });
 
 // const PORT = app.get("PORT") || 4000;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is running ....${PORT}`);
 });
